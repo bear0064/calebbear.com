d2eca3240eed2ea2f2b0e8f16410d2930a1e593c
bear6310 <bear0064@algonquinlive.com>
2014-12-15 19:31:54 -0500

    Con't Fixes
    Adapted header, fixed styles

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", function() {
+$("#navbar").hide().fadeIn(3000);
+
+
+var scroll_start = 0;
+                var startchange = $('#startchange');
+                var offset = startchange.offset();
+                    if (startchange.length){
+                $(document).scroll(function() { 
+                    scroll_start = $(this).scrollTop();
+                    if(scroll_start > offset.top) {
+                          $(".navbar-default").css('background-color', '#000');
+                       } else {
+                          $('.navbar-default').css('background-color', 'transparent');
+                       }
+                   });
+                    }
+
+    
+
+
+
+});
+
+
