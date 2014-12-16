1e6b02c0aa4f28199dbf9904f68a79405b982881
bear6310 <bear0064@algonquinlive.com>
2014-12-16 01:33:18 -0500

    Fixed Fade effect on Header
    added JS function to fade Header on Scroll

--- js/javascript.js
+++ js/javascript.js
@@ -1,29 +1,42 @@
-window.addEventListener("DOMContentLoaded", function() {
-$("#navbar").hide().fadeIn(3000);
+window.addEventListener("DOMContentLoaded", function () {
+    $("#navbar").hide().fadeIn(3000);
 
-$(function(){
-  $.scrollIt();
+    $(function () {
+        $.scrollIt();
+    });
+
+
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 50) {
+        $('.navbar-default').addClass('opaque');
+    } else {
+        $('.navbar-default').removeClass('opaque');
+    }
 });    
     
-
-var scroll_start = 0;
-                var startchange = $('#startchange');
-                var offset = startchange.offset();
-                    if (startchange.length){
-                $(document).scroll(function() { 
-                    scroll_start = $(this).scrollTop();
-                    if(scroll_start > offset.top) {
-                          $(".navbar-default").css('background-color', '#000');
-                       } else {
-                          $('.navbar-default').css('background-color', 'transparent');
-                       }
-                   });
-                    }
-
     
+    
+/*
 
-
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $(".navbar-default").css('background-color', '#000');
+            } else {
+                $('.navbar-default').css('background-color', 'transparent');
+            }
+        });
+    }
+*/
 
 });
 
 
+
+
+                    
