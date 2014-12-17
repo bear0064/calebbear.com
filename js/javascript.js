b0f7b7652b008eb27103f99f38ea1829e4120ada
bear6310 <bear0064@algonquinlive.com>
2014-12-17 03:06:20 -0500

    Clean up JS

--- js/javascript.js
+++ js/javascript.js
@@ -7,43 +7,19 @@ window.addEventListener("DOMContentLoaded", function () {
 
 
 
-$(window).scroll(function() {
-    if($(this).scrollTop() > 25) {
-        $('.navbar-default').addClass('opaque');
-    } else {
-        $('.navbar-default').removeClass('opaque');
-    }
-});    
-    
-    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 25) {
+            $('.navbar-default').addClass('opaque');
+        } else {
+            $('.navbar-default').removeClass('opaque');
+        }
+    });
 
-    
-$('.nav a').on('click', function(){
-    $(".btn-navbar").click(); //bootstrap 2.x
-    $(".navbar-toggle").click() //bootstrap 3.x by Richard
-});
- 
-    
-/*
+    $('.nav a').on('click', function () {
+        $(".btn-navbar").click(); //bootstrap 2.x
+        $(".navbar-toggle").click() //bootstrap 3.x by Richard
+    });
 
-    var scroll_start = 0;
-    var startchange = $('#startchange');
-    var offset = startchange.offset();
-    if (startchange.length) {
-        $(document).scroll(function () {
-            scroll_start = $(this).scrollTop();
-            if (scroll_start > offset.top) {
-                $(".navbar-default").css('background-color', '#000');
-            } else {
-                $('.navbar-default').css('background-color', 'transparent');
-            }
-        });
-    }
-*/
+
 
 });
-
-
-
-
-                    
