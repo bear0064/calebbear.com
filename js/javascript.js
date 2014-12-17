a340ca2806e5c07256181f22c6f64af114fde116
bear6310 <bear0064@algonquinlive.com>
2014-12-17 03:06:53 -0500

    Remove Excess Code

--- js/javascript.js
+++ js/javascript.js
@@ -16,8 +16,7 @@ window.addEventListener("DOMContentLoaded", function () {
     });
 
     $('.nav a').on('click', function () {
-        $(".btn-navbar").click(); //bootstrap 2.x
-        $(".navbar-toggle").click() //bootstrap 3.x by Richard
+        $(".navbar-toggle").click() 
     });
 
 
