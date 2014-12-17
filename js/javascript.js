1587b2effe03ca3539def21ce1015c8b04b9647c
bear6310 <bear0064@algonquinlive.com>
2014-12-17 03:05:33 -0500

    mobile nav bar changes
    Added responsiveness to nav bar on click (toggles the menu)

--- js/javascript.js
+++ js/javascript.js
@@ -16,6 +16,13 @@ $(window).scroll(function() {
 });    
     
     
+
+    
+$('.nav a').on('click', function(){
+    $(".btn-navbar").click(); //bootstrap 2.x
+    $(".navbar-toggle").click() //bootstrap 3.x by Richard
+});
+ 
     
 /*
 
